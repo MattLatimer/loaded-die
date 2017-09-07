b7f3933810dedf76ed07401836d3f536f5939f6c
Matt <matt.latimer@gmail.com>
2017-09-07 21:31:19 +0000

    added functions to create loaded dice

--- loaded-die.js
+++ loaded-die.js
@@ -1,10 +1,11 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  var current = list.length - 1;
 
   return function() {
-    /* your code here */
-  }
+    current = (current + 1) % list.length;
+    return list[current];
+  };
 }
 
 var rollLoadedDie = makeLoadedDie();
